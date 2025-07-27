9a06fa4b4de837698a68120d9bc1b16b60155da9
Samson Odejinmi <odejinmisamson37@gmail.com>
2025-07-27 22:52:25 +0100

    Update

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Qzlc4bUjmZmJ1BnHodWeJ7eR9uTJNUI",
+  authDomain: "socialnet-1aa9f.firebaseapp.com",
+  projectId: "socialnet-1aa9f",
+  storageBucket: "socialnet-1aa9f.firebasestorage.app",
+  messagingSenderId: "824507729261",
+  appId: "1:824507729261:web:a5319abc01c0811e168664",
+  measurementId: "G-GTCJBW4GEM"
+};
